a1854d760cdf164dd2218a57dbeefe80977626bc
3ireborn <pakdsugie3i@gmail.com>
2026-06-19 11:13:48 +0700

    Update leader.js

--- leader.js
+++ leader.js
@@ -1,14 +1,81 @@
 const leaders = {
-  "sugiarto": "6285218453131",     // Sugiarto Kurniawan
-  "agnes": "6285932684440",        // Agnes Marsilah
-  "suwarti": "62881011823140",     // Suwarti
-  "iyang": "6281388675615",        // Iyang
-  "queen": "6282199558169",        // Queen
-  "rahmawan": "6281327498929",     // Rahmawan
-  "regina": "6281285949074",       // Regina
-  "tasya": "966563414575",         // Tasya
-  "divawati": "6586210326",        // Divawati
-  "tohir": "6281236544821",        // Tohir
-  "alpiano": "6282252161911",      // Alpiano
-  "default": "6285218453131"       // Default ke Sugiarto (WA masuk ke dia kalau link tanpa ?leader=)
+
+  "sugiarto": {
+    nama: "PakD Sugiarto Kurniawan",
+    wa: "6285218453131",
+    title: "Pendekar Bertangan Satu"
+  },
+
+  "agnes": {
+    nama: "Agnes Marsilah",
+    wa: "6285932684400",
+    title: "Mitra 3IReborn"
+  },
+  
+"tini": {
+    nama: "Queen Tini Tabrani",
+    wa: "6281318338844",
+    title: "Queen 3IReborn"
+  },
+  
+  "suwarti": {
+    nama: "Suwarti",
+    wa: "62881011823140",
+    title: "Mitra 3IReborn"
+  },
+
+  "iyang": {
+    nama: "Iyang",
+    wa: "6281388675615",
+    title: "Mitra 3IReborn"
+  },
+
+  "queen": {
+    nama: "Queen",
+    wa: "628199558169",
+    title: "Mitra 3IReborn"
+  },
+
+  "rahmawan": {
+    nama: "Rahmawan",
+    wa: "628132749829",
+    title: "Mitra 3IReborn"
+  },
+
+  "regina": {
+    nama: "Regina",
+    wa: "628128549074",
+    title: "Mitra 3IReborn"
+  },
+
+  "tasya": {
+    nama: "Tasya",
+    wa: "966563414575",
+    title: "Mitra 3IReborn"
+  },
+
+  "divawati": {
+    nama: "Divawati",
+    wa: "6586210326",
+    title: "Mitra 3IReborn"
+  },
+
+  "tohir": {
+    nama: "Tohir",
+    wa: "628123654821",
+    title: "Mitra 3IReborn"
+  },
+
+  "alpiano": {
+    nama: "Alpiano",
+    wa: "6282252161911",
+    title: "Mitra 3IReborn"
+  },
+
+  "default": {
+    nama: "PakD Sugiarto Kurniawan",
+    wa: "6285218453131",
+    title: "3IReborn Digital System"
+  }
+
 };
