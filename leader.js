865a850cf267ce24c36e74c80803976f264818e7
3ireborn <pakdsugie3i@gmail.com>
2026-06-21 14:48:55 +0700

    Update leader.js

--- leader.js
+++ leader.js
@@ -74,19 +74,19 @@ const leaders = {
   
 "kijang":{
 nama:"Kijang",
-wa:"6285314645611"
+wa:"6285314645611",
 title: "Mitra 3IReborn"
 },
 
 "kombet":{
 nama:"Kombet",
-wa:"6282221978889"
+wa:"6282221978889",
 title: "Mitra 3IReborn"
 },
 
 "lutfi":{
 nama:"Lutfi",
-wa:"6283893388848"
+wa:"6283893388848",
 title: "Mitra 3IReborn"
 },
   
